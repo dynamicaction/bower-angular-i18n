4cedff7984f4fc2041c4260891f873c56250b5f8
Svetoslav Rankov <svetoslav.rankov@orderdynamics.com>
2015-04-30 16:34:43 +0300

    Change the negative sign to brackets

--- angular-locale_en-gb.js
+++ angular-locale_en-gb.js
@@ -129,8 +129,8 @@ $provide.value("$locale", {
         "maxFrac": 2,
         "minFrac": 2,
         "minInt": 1,
-        "negPre": "-\u00a4",
-        "negSuf": "",
+        "negPre": "(\u00a4",
+        "negSuf": ")",
         "posPre": "\u00a4",
         "posSuf": ""
       }
